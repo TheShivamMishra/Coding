36fd78b56ef7530210d2d1501869a1b8bde9fd50
Shivam Mishra <36327761+dudeshivam@users.noreply.github.com>
2020-08-10 16:34:36 +0530

    updated

--- LinkedList/question.java
+++ LinkedList/question.java
@@ -196,18 +196,18 @@ public class ListNode {
         return head.next;
     }
      
-    ListNode mergeTwoLists(Node* l1, Node* l2) {
+    Node mergeTwoLists(Node l1, Node l2) {
         if (l1 == null) {
             return l2;
         } else if (l2 == null) {
             return l1;
         }
 
-        if (l1->val <= l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
-            l2->next = mergeTwoLists(l1, l2->next);
+            l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
 
